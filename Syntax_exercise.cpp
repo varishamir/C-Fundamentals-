6c21bc94be155501a65c6dba33bb16a1dc766b3d
varishamir <varimir234@gmail.com>
2026-02-14 00:10:12 +0500

    Create Syntax_exercise.cpp

---
+++ Syntax_exercise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"enter_number= ";
+	cin>>number;
+	if(number%2==0)
+	{ cout<<"This number is even";
+	}
+	else
+	{ cout<<"This number is odd";
+	}
+}
